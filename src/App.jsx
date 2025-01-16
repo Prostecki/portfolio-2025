69c410a8f0459c4b3dbe50020c06b9d3cbf2f3a7
Mark Taratynov <mark.taratynov@gmail.com>
2025-01-16 21:03:56 +0100

    Implemented intersection oberver api for testing of scrolling the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,38 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import TechStack from "./components/TechStack";
+import { useRef, useEffect } from "react";
 
 function App() {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log("Элемент виден!");
+          } else {
+            console.log("Элемент не виден");
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    const currentElement = elementRef.current;
+    if (currentElement) observer.observe(currentElement);
+
+    return () => {
+      if (currentElement) observer.unobserve(currentElement);
+    };
+  }, []);
   return (
     <>
       <Header />
       <main className="flex flex-col items-center">
         <Home />
-        <About />
+        <About ref={elementRef} />
         <Experience />
         <TechStack />
       </main>
