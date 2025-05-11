21a76930791f914f2e1a7cfab8cc0e9f1b17695b
Mark Taratynov <mark.taratynov@gmail.com>
2025-05-11 21:07:27 +0200

    Refactor animation properties in Home component for improved performance and consistency. Adjusted duration and delay values for motion elements.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,8 +19,8 @@ export default function Home({ scrollTo }) {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.5,
-        delay: custom * 0.45,
+        duration: 0.3,
+        delay: custom * 0.4,
       },
     }),
   };
@@ -46,7 +46,7 @@ export default function Home({ scrollTo }) {
         className="flex flex-col gap-6 items-center justify-center"
       >
         <motion.span
-          custom={0}
+          custom={0.1}
           variants={itemVariants}
           className="inline-block py-1 px-3 rounded-full text-sm font-medium dark:bg-teal-500/10 dark:text-blue-500 bg-slate-300/30 drop-shadow-2xl text-slate-800/60 mb-4"
         >
@@ -54,7 +54,7 @@ export default function Home({ scrollTo }) {
         </motion.span>
 
         <motion.div
-          custom={1}
+          custom={0.2}
           variants={itemVariants}
           className="relative rounded-full w-64 h-64 flex items-center justify-center"
         >
@@ -70,7 +70,7 @@ export default function Home({ scrollTo }) {
             }}
           />
           <img
-            custom={1.5}
+            custom={0.3}
             variants={itemVariants}
             src="/images/me.jpeg"
             alt="Avatar"
@@ -79,7 +79,7 @@ export default function Home({ scrollTo }) {
         </motion.div>
 
         <motion.h1
-          custom={2}
+          custom={0.4}
           variants={itemVariants}
           style={{
             textShadow:
@@ -94,7 +94,7 @@ export default function Home({ scrollTo }) {
         </motion.h1>
 
         <motion.div
-          custom={3}
+          custom={0.5}
           variants={itemVariants}
           className="flex relative items-center justify-between gap-2 border dark:border-slate-300/40 border-black/50 rounded-xl px-4 py-2"
         >
@@ -108,7 +108,7 @@ export default function Home({ scrollTo }) {
         </motion.div>
 
         <motion.h2
-          custom={4}
+          custom={0.6}
           variants={itemVariants}
           className="text-2xl dark:bg-gradient-to-r dark:from-white dark:to-gray-400 dark:text-transparent dark:bg-clip-text font-bold text-balance text-center text-slate-800/80"
         >
@@ -116,7 +116,7 @@ export default function Home({ scrollTo }) {
         </motion.h2>
 
         <motion.p
-          custom={5}
+          custom={0.7}
           variants={itemVariants}
           className="text-balance dark:text-slate-100 text-slate-400 text-center"
         >
@@ -127,12 +127,12 @@ export default function Home({ scrollTo }) {
         </motion.p>
 
         <motion.div
-          custom={6}
+          custom={0.8}
           variants={itemVariants}
           className="flex gap-5 w-max"
         >
           <motion.a
-            custom={6.1}
+            custom={0.9}
             variants={itemVariants}
             href="https://taratynov-cv-page.vercel.app/"
             target="_blank"
@@ -146,7 +146,7 @@ export default function Home({ scrollTo }) {
             />
           </motion.a>
           <motion.button
-            custom={6.2}
+            custom={1.0}
             variants={itemVariants}
             onClick={() => scrollToSection(scrollTo.getInTouchRef)}
             className="border focus-visible:ring-2 border-slate-400 bg-white text-slate-800 w-max px-4 rounded-lg shadow-md py-2 hover:bg-slate-200 duration-300"
